5cfa881478faa2c69a0afb03dc25025a800029c9
smirnovice <asmirnov@tcd.ie>
2017-11-01 20:19:01 +0000

    Updated DAG code, first one was incorrect

--- LowestCommonAncestor/src/DAG.java
+++ LowestCommonAncestor/src/DAG.java
@@ -1,171 +1,112 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class DAG<Key extends Comparable<Key>, Value> {
-		public Node root; 
-	
-		/**
-		 * Private node class.
-		 */
-		private class Node 
-		{
-			List<Node> successor;
-			private Key key;
-	
-			public Node(Key key) 
-			{
-				this.key = key;
-			}
-		}
-	
-		public Node addNode(Key key) 
-		{
-			if (root == null)
-				return root = new Node(key);
-			if (root.successor == null)
-				root.successor = new ArrayList<Node>();
-	
-			Node tmp = new Node(key);
-			root.successor.add(tmp);
-			return tmp;
-		}
-	
-		public Node connectNew(Key key, Key newNode) 
-		{
-			Node find = search(key);
-			if (find.successor == null)
-				find.successor = new ArrayList<Node>();
-			Node tmp = new Node(newNode);
-			find.successor.add(tmp);
-			return tmp;
-		}
-	
-		public Node search(Key key) 
-		{
-			if (root.key == key)
-			{
-				return root;
-			}
-			return search(root, key,root);
-		}
-	
-		private Node search(Node find, Key key, Node ans) 
-		{
-			
-			if (find.key == key)
-			{
-			  ans=find;
-			  return ans;
-			}
-			int i = 0;
-			if (find.successor != null) 
-			{
-				while ((i) < find.successor.size()) 
-				{		
-					ans=search(find.successor.get(i), key,ans);
-					 i++;
-				}
-			}
-		    return ans;
-		}
-		
-		public boolean connect(Node first, Node second)
-		{
-			boolean toCheck=false;	
-			toCheck=connectCheck(root, first, second, 0,true);
-			if(toCheck)
-			{
-				if (first.successor == null)
-				{
-					first.successor = new ArrayList<Node>();
-				first.successor.add(second);
-				return true;
-				}
-			}
-			return false;
-		}
-		private boolean connectCheck(Node head, Node first, Node second, int count, boolean ans)
-		{
-			int i = -1;
-			if(head.key==second.key)
-			{	
-				count++;
-			}
-			if(count>0&&head.key==first.key)
-			{				
-				ans= false;
-				return ans;
-			}
-			if (head.successor != null) 
-			{
-				
-				while ((i + 1) < head.successor.size()) 
-				{
-					i++;
-					 ans=connectCheck(head.successor.get(i), first, second,count,ans);
-				}	
-			}
-			
-			return ans;
-		}
-		public Node LCA (Node first, Node second)
-		{
-			if(first.key==second.key)
-				return first;
-			return LCA(root,first, second,root);
-		}
-		private Node LCA(Node head, Node first, Node second, Node ans)
-		{
-			int i = -1;
-		    if(head.key==first.key)
-		    {
-		    	ans=searchLCA(first,second.key,ans,first);
-		    	return ans;
-		    }
-		    if(head.key==second.key)
-		    {
-		    	ans=searchLCA(second,first.key,ans,second);
-		    	return ans;
-		    }
-			if (head.successor != null) 
-			{	
-				while ((i + 1) < head.successor.size()) 
-				{
-					i++;
-					ans=LCA(head.successor.get(i), first, second,ans);
-				}	
-			}
-			
-			return ans;
-		}
-	private Node searchLCA(Node find, Key key, Node ans, Node start) 
+
+public class DAG 
+{
+	public Node head;
+	public class Node
 	{
-			
-			if (find.key == key)
-			{
-			  ans=start;
-			  return ans;
-			}
-			int i = 0;
-			if (find.successor != null) 
-			{
-				while ((i) < find.successor.size()) 
-				{	
-					ans=searchLCA(find.successor.get(i), key,ans, start);
-					 i++;
-				}
-			}
-		    return ans;
-		}
-	
-	public static void main(String[] args) 
+		char key;
+		int val;
+		List<Node> parents = new ArrayList<Node>();
+		List<Node> children = new ArrayList<Node>();
+	}
+
+	public void createHead(int val, char key)
 	{
-		DAG<Integer, Integer> bst = new DAG<Integer, Integer>();
-		bst.addNode(1);
-		bst.addNode(2);
-		bst.addNode(3);
-		bst.connectNew(3, 4);
-		System.out.println(bst.LCA(bst.search(3),bst.search(2)).key);
+		Node head = new Node();
+		head.val = val;
+		head.key = key;
+		head.parents = new ArrayList<Node>();
+		head.children = new ArrayList<Node>();
+		this.head = head;
 	}
 	
+	public int returnVal(Node node)
+	{
+		return node.val;
+	}
+	
+	public char returnKey(Node newN)
+	{
+		return newN.key;
+	}
+	
+	public void addNode(char key, int val, Node parent)
+	{
+		Node newNode = new Node();
+		newNode.key = key;
+		newNode.children = new ArrayList<Node>();
+		newNode.val = val;
+		newNode.parents.add(parent);
+		parent.children.add(newNode);
+	}
+	
+	public Node findNode(char key)
+	{
+		Node x;
+		if (key == head.key)
+		{
+			return head;
+		}
+		else
+		{
+			for(int i = 0; i < head.children.size();i++)
+			{
+				x = findNode(key,head.children.get(i));
+				if (x != null)
+				{
+					return x;
+				}
+			}
+		}
+		return null;
+	}
+	
+	private Node findNode(char key, Node a)
+	{
+		Node x;
+		if(a.key == key){
+			return a;
+		}
+		else
+		{
+			for(int i = 0; i < a.children.size();i++)
+			{
+				x = findNode(key,a.children.get(i));
+				if (x != null)
+				{
+					return x;
+				}
+			}
+		}
+		return null;
+	}
+	
+	public Node LCA(Node start, Node a, Node b)
+	{
+		Node x = null;
+		if (a.key == head.key || b.key == head.key)
+		{
+			return head;
+		}
+		else
+		{
+			Node tempA = findNode(a.key, start);
+			Node tempB = findNode(b.key, start);
+			if (tempA != null && tempB != null)
+			{
+				x = start;
+				for(int i = 0; i < start.children.size();i++)
+				{
+					if (LCA(start.children.get(i),a,b) != null)
+					{
+						x = LCA(start.children.get(i),a,b);
+					}
+				}				
+			}			
+		}
+		return x;
+	}
 }
